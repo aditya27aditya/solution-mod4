3138e18419e80955d7a7f16fec6745452a3c5e21
aditya joshi <aditya27aditya@gmail.com>
2020-06-17 19:01:28 +0530

    save

--- module4_solution/script.js
+++ module4_solution/script.js
@@ -16,7 +16,7 @@ Good Bye Jim
 */
 
 (function() {
-    var names = ["Yaakov", "Jacob", "Jai", "Jason", "Paul", "Frank", "Laxmi", "Paula", "Rahul", "Jim", "Aditya", "Sharma"];
+    var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
     for (var i = 0; i < names.length; i++) {
         var firstLetter = names[i].charAt(0).toLowerCase();
         if (firstLetter === 'j') {
